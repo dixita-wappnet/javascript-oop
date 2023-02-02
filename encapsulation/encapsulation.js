d0e5abc055767215f7d63cb9503f0f6cf4655646
Dixita Patel <dixita.wappnet@gmail.com>
2023-02-02 16:46:56 +0530

    encapsulation

---
+++ encapsulation/encapsulation.js
@@ -0,0 +1,29 @@
+
+    <script>
+
+     class e1
+     
+        constructor(name,id)
+        
+            this.name = nam;
+            this.id = id;
+        
+        hit(add)
+        
+            this.add = add;
+        
+
+        fig()
+        
+            document.write(` The user name is :- ${this.name}  Address is :- ${this.add}`);
+        
+
+     
+
+     let student1 = new e1('Dixita Patel', 'Surat');
+     
+     student1.hit('Surat');
+     student1.fig();
+
+    </script>
+    
